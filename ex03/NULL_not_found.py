5e71d5572ec36d5c2e4b37fe4cc1b48826d1417f
Lyfmeno <hugolevipro@gmail.com>
2023-07-28 15:08:35 +0200

    ex03 done

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -2,26 +2,16 @@
 
 def NULL_not_found(object : any) -> int:
     if object is None:
-        if type(object) is list:
-            print ("List :", object, type(object))
-        elif type(object) is set:
-            print ("Set :", object, type(object))
-        elif type(object) is dict:
-            print ("Dict :", object, type(object))
-        elif type(object) is str:
-            print ("Brian is in the kitchen :", object, type(object))
-        elif type(object) is tuple:
-            print ("Tuple :", object, type(object))
+        print ("Nothing :", object, type(object))
+    if object is False:
+        print ("Fake :", object, type(object))
+    elif object == "":
+        print ("Empty :", object, type(object))
+    elif object == 0:
+        print ("Zero :", object, type(object))
+    elif isinstance(object, float):
+        print ("Cheese :", object, type(object))
+    else:
+        print ("Type not Found")
+        return 1
     return 0
-
-Nothing = None
-Garlic = float("NaN")
-Zero = 0
-Empty = ''
-Fake = False
-NULL_not_found(Nothing)
-NULL_not_found(Garlic)
-NULL_not_found(Zero)
-NULL_not_found(Empty)
-NULL_not_found(Fake)
-print(NULL_not_found("Brian"))
