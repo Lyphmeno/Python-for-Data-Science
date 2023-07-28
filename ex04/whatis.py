9e7059df8327869dac482e2e959804293739c551
Lyfmeno <hugolevipro@gmail.com>
2023-07-28 16:50:03 +0200

    ex04 done

---
+++ ex04/whatis.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.10
+
+import sys
+
+def is_int(number):
+    try:
+        int(number)
+        return True
+    except  ValueError:
+        return False
+
+def odd_or_even():
+    if len(sys.argv) < 2:
+        return 1
+    try:
+        assert len(sys.argv) == 2, "AssertionError: more than one argument is provided"
+        assert is_int(sys.argv[1]), "Assertion error: argument is  not an integer"
+        if int(sys.argv[1]) % 2 != 0:
+            print("I'm Odd.")
+        else:
+            print("I'm Even.")
+    except AssertionError as error:
+        if error != "":
+            print(error)
+
+odd_or_even()
