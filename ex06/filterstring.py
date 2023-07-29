8356253daf80ef70aaa1adfcced7c07592420aed
Lyfmeno <hugolevipro@gmail.com>
2023-07-29 16:33:50 +0200

    ex06 done - starting ex07

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3.10
+
+import sys
+import ft_filter
+
+
+def isint(number):
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+
+def isalphasp(str):
+    return all(c.isalpha() or c.isspace() for c in str)
+
+
+def main():
+    try:
+        assert len(sys.argv) == 3, "AssertionError: the arguments are bad"
+        tp = type(sys.argv[1])
+        assert tp == str, "AssertionError: the arguments are bad"
+        assert isalphasp(sys.argv[1]), "AssertionError: the arguments are bad"
+        assert isint(sys.argv[2]), "AssertionError: the arguments are bad"
+        print(ft_filter.ft_filter(lambda word: len(word) > int(sys.argv[2]),
+                                  sys.argv[1].split()))
+    except AssertionError as err:
+        print(err)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.10
+
+
+def ft_filter(func, iter):
+    """The ft_filter() function selects elements from an iterable \
+(list, tuple etc.) based on the output of a function.\
+The function is applied to each element of the iterable and if it returns \
+True, the element is selected by the ft_filter() function."""
+    new = [x for x in iter if func(x) is True]
+    return new
