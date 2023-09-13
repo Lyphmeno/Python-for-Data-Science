b6b16d2e5dd13bd67ff5c8313e992dd1a6a3bb41
Lyfmeno <hugolevipro@gmail.com>
2023-09-13 16:47:43 +0200

    bmi corrected - ex00 DONE

--- 1 - Array/ex00/give_bmi.py
+++ 1 - Array/ex00/give_bmi.py
@@ -49,7 +49,7 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """
     if not all(isinstance(i, int | float) for i in bmi):
         raise TypeError("'bmi' elements must be int of float")
-    if limit is not int | float:
+    if not isinstance(limit, int):
         raise TypeError("limit must be int of float")
     res = []
     for i in bmi:
@@ -58,3 +58,10 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
         else:
             res.append(False)
     return res
+
+
+height = [2.71, 1.15]
+weight = [165.3, 38.4]
+bmi = give_bmi(height, weight)
+print(bmi, type(bmi))
+print(apply_limit(bmi, 26))
