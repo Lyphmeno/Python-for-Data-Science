a11c95ade9289f431df9debcccfc79f4c3ab6217
Lyfmeno <hugolevipro@gmail.com>
2023-09-13 17:08:29 +0200

    better tranpose without lib - ex04 DONE

--- 1 - Array/ex04/rotate.py
+++ 1 - Array/ex04/rotate.py
@@ -2,24 +2,7 @@
 
 from PIL import Image
 from load_image import ft_load
-
-
-def ft_transpose(image):
-    """
-    Rotate an image (array) by transposing its dimensions.
-
-    Args:
-        image: The input image as array.
-
-    Returns:
-        img_transposed: The transposed image/array.
-
-    Raises:
-        None
-    """
-    h, w, channels = image.shape
-    img_transposed = image.transpose(1, 0, 2)
-    return img_transposed
+import numpy as np
 
 
 def main():
@@ -37,18 +20,12 @@ def main():
         img = ft_load("animal.jpeg")
         if img is None:
             raise FileNotFoundError("Loading the image failed")
-        h, w, channels = img.shape
-        size = min(h, w)
-        start_x = 0
-        start_y = 0
-        square_arr = img[start_y:start_y + size, start_x:start_x + size, :]
-        newarr = ft_transpose(square_arr)
-        print("The shape of the image is:", img.shape)
-        print("New shape after Transpose:", newarr.shape)
-        newimg = Image.fromarray(newarr)
-        newimg.show()
+        newimg = img[100:500, 450:850]
+        newimg = np.array(list(zip(*newimg)))
+        print("New shape after slicing:", newimg.shape, "\n", newimg)
+        Image.fromarray(newimg).show()
     except FileNotFoundError as e:
-        print(f"Error: {e}")
+        print(f"{e}")
 
 
 if __name__ == "__main__":
