e532ca2719f0c02628687103a28f21a610775385
Lyfmeno <hugolevipro@gmail.com>
2023-09-13 17:36:28 +0200

    ex05 DONE

--- 1 - Array/ex05/pimp_image.py
+++ 1 - Array/ex05/pimp_image.py
@@ -4,60 +4,36 @@ import numpy as np
 
 
 def ft_invert(array) -> np.array:
-    """
-    This function invert the color of an image by changing the rgb channel.
-
-    :param array: array image
-    :returns: Returns the modified array
-    :raises: /
-    """
-    return np.mqx(array) - array
+    """Inverts the color of the image received."""
+    return 255 - array
 
 
 def ft_red(array) -> np.array:
-    """
-    This function apply a red filter on an image by changing the rgb channel.
-
-    :param array: array image
-    :returns: Returns the modified array
-    :raises: /
-    """
-    array[:, :, 0] = 255
-    return array
+    """Apply a red filter on an image by changing the rgb channel."""
+    newarr = array.copy()
+    newarr[:, :, 1] = 0
+    newarr[:, :, 2] = 0
+    return newarr
 
 
 def ft_green(array) -> np.array:
-    """
-    This function apply a red filter on an image by changing the rgb channel.
-
-    :param array: array image
-    :returns: Returns the modified array
-    :raises: /
-    """
-    array[:, :, 1] = 255
-    return array
+    """Apply a green filter on an image by changing the rgb channel."""
+    newarr = array.copy()
+    newarr[:, :, 0] = 0
+    newarr[:, :, 2] = 0
+    return newarr
 
 
 def ft_blue(array) -> np.array:
-    """
-    This function apply a blue filter on an image by changing the rgb channel.
-
-    :param array: array image
-    :returns: Returns the modified array
-    :raises: /
-    """
-    array[:, :, 2] = 255
-    return array
+    """Apply a blue filter on an image by changing the rgb channel."""
+    newarr = array.copy()
+    newarr[:, :, 0] = 0
+    newarr[:, :, 1] = 0
+    return newarr
 
 
 def ft_grey(array) -> np.array:
-    """
-    This function apply a grey filter on an image by changing the rgb channel.
-
-    :param array: array image
-    :returns: Returns the modified array
-    :raises: /
-    """
+    """Apply a grey filter on an image by changing the rgb channel."""
     val = np.mean(array, axis=2, dtype=int)
     newarr = np.zeros_like(array)
     for i in range(3):
