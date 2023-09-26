1ad2d6ee45afec864d0ca30a324d9b47db28868f
Lyfmeno <hugolevipro@gmail.com>
2023-09-26 12:54:56 +0200

    ex00 DONE - removed test

--- 4 - DOD/ex00/statistic.py
+++ 4 - DOD/ex00/statistic.py
@@ -47,12 +47,3 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
                 print(f"var : {res}")
     except Exception as e:
         print(f"ERROR: {e}")
-
-
-ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
-print("-----")
-ft_statistics(5, 75, 450, 18, 597, 27474, 48575, hello="std", world="var")
-print("-----")
-ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
-print("-----")
-ft_statistics(toto="mean", tutu="median", tata="quartile")
